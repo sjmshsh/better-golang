3c0dc31bb277cdccaf236a8e0beb14a9d9667e51
lixinyang06 <lixinyang06@kuaishou.com>
2024-07-13 18:05:18 +0800

    solve

--- mem_leak/main.go
+++ mem_leak/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 var s0 string // a package-level variable
 
 func f(s1 string) {
@@ -10,6 +12,21 @@ func f(s1 string) {
 	// couldn't be collected, though there are only 50
 	// bytes used in the block and all other bytes in
 	// the block become unavailable.
+}
+
+// 为了避免这种内存泄漏，我们可以将子字符串转换为一个 []byte值，然后将该[]byte值转换回string。
+func f1(s1 string) {
+	s0 = string([]byte(s1[:50]))
+}
+
+// 编译器优化之后可能会变得无效
+func f2(s1 string) {
+	s0 = (" " + s1[:50])[1:]
+}
+
+// Go1.18 开始提供了Clone来进行克隆字符串
+func f3(s1 string) {
+	s0 = strings.Clone(s1)
 }
 
 func demo() {
