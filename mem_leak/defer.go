b13fbfe2457a676d8cedf3bf88a2a5ab71b54ef5
lixinyang06 <lixinyang06@kuaishou.com>
2024-07-13 18:15:59 +0800

    defer leak

---
+++ mem_leak/defer.go
@@ -0,0 +1,51 @@
+package main
+
+import "os"
+
+type File struct {
+	path    string
+	content string
+}
+
+func writeManyFiles(files []File) error {
+	for _, file := range files {
+		f, err := os.Open(file.path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		_, err = f.WriteString(file.content)
+		if err != nil {
+			return err
+		}
+
+		err = f.Sync()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func shouldWriteManyFiles(files []File) error {
+	for _, file := range files {
+		if err := func() error {
+			f, err := os.Open(file.path)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			_, err = f.WriteString(file.content)
+			if err != nil {
+				return err
+			}
+
+			return f.Sync()
+		}(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
